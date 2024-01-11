6c9494c72aaa27e7f18b3c0684947d47a3781e06
jjomi <j.hist26@gmail.com>
2024-01-11 18:40:22 +0900

    [style] 회원탈퇴 페이지 연결

--- src/pages/InfoEdit.jsx
+++ src/pages/InfoEdit.jsx
@@ -1,38 +1,26 @@
 // InfoEdit.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+
 import Header from "../components/layout/Header";
-import {
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  FormLabel,
-  Grid,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import format from "date-fns/locale/ko";
+import { Box, Button, Container, Grid, Typography, Link } from "@mui/material";
 
 import InputField from "../components/common/InputField";
-import { SignInStyles } from "./PageStyles";
-import { datePickerContainer, datePicker } from "./info/InfoStyles";
 import DuplicateCheckButton from "../components/common/DuplicateCheckButton";
-
 import DatePickerField from "../components/common/DatePickerField";
+import GenderBtn from "../components/common/GenderBtn";
+import { SignInStyles } from "./PageStyles";
 
 export default function InfoEdit() {
   const navigate = useNavigate();
-  // const [nickname, birthday, sex] = ["지렁이", "2111-01-11", "여"];
 
   const [nickname, setNickname] = useState("");
   const [birthday, setBirthday] = useState(null); // or some default date
   const [sex, setSex] = useState(""); // or
   const [jobSkill, setJobSkill] = useState("");
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [value, setValue] = useState({ nickname, birthday, sex });
+  // const [selectedDate, setSelectedDate] = useState(null);
+
   const [isNicknameValid, setNicknameValid] = useState(true);
 
   // 초반에 정보들 넣어주기
@@ -51,20 +39,21 @@ export default function InfoEdit() {
     //통신에서 성공하면 변경해주고아니면 에러 창 띄워주기
   };
   const handleDateChange = (newValue) => {
-    setSelectedDate(newValue);
+    setBirthday(newValue);
     handleInputChange("birthday", newValue);
   };
-
+  const handleSexChange = (selectedGender) => {
+    console.log("성별 선택", selectedGender);
+  };
   const handleSaveChanges = () => {
     // 여기서 변경된 정보를 저장하는 로직을 추가
     // 저장이 완료되면 프로필 페이지로 이동
-    navigate("/profile");
+    navigate("/mypage");
   };
 
   return (
     <div>
       <Header showBackButton={true} myText={"정보수정"} />
-
       <Container component="main" sx={{ marginTop: 6, paddingX: "29px" }}>
         <form onSubmit={handleSaveChanges}>
           <InputField
@@ -87,54 +76,60 @@ export default function InfoEdit() {
               ),
             }}
           />
-          {/* <Box>
-            <FormLabel>생일</FormLabel>
-            <Grid container sx={datePickerContainer(value.birthday)}>
-              <LocalizationProvider
-                dateAdapter={AdapterDateFns}
-                adapterLocale={format}
-              >
-                <DatePicker
-                  value={selectedDate}
-                  inputFormat="yyyy.MM.dd"
-                  onChange={handleDateChange}
-                  sx={datePicker(value.birthday)}
-                  renderInput={(params) => (
-                    <TextField {...params} fullWidth sx={{ flexGrow: 1 }} />
-                  )}
-                />
-              </LocalizationProvider>
+          <Grid container spacing={4.5}>
+            <Grid item xs={12}>
+              <DatePickerField
+                value={birthday}
+                selectedDate={birthday}
+                onChange={handleDateChange}
+              />
             </Grid>
-          </Box> */}
-          <DatePickerField
-            value={selectedDate}
-            selectedDate={selectedDate}
-            onChange={handleDateChange}
-          />
-
-          <InputField
-            label="직무 및 기술 스택"
-            type="standard"
-            id="jobSkill"
-            name="jobSkill"
-            autoComplete="jobSkill"
-            placeholder="클릭하여 선택하기"
-            value={jobSkill}
-            onChange={handleInputChange}
-            error={!jobSkill}
-            helperText={!jobSkill ? "직무 및 기술 스택을 선택해주세요" : ""}
-          />
-
-          <Button
-            variant="contained"
-            fullWidth
-            color="primary"
-            onClick={handleSaveChanges}
-            sx={SignInStyles.Button}
-          >
-            수정
-          </Button>
+            <Grid item xs={12}>
+              <GenderBtn handleSexChange={handleSexChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <InputField
+                label="직무 및 기술 스택"
+                type="standard"
+                id="jobSkill"
+                name="jobSkill"
+                autoComplete="jobSkill"
+                placeholder="클릭하여 선택하기"
+                value={jobSkill}
+                onChange={handleInputChange}
+                error={!jobSkill}
+                helperText={!jobSkill ? "직무 및 기술 스택을 선택해주세요" : ""}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                onClick={handleSaveChanges}
+                sx={SignInStyles.Button}
+              >
+                수정
+              </Button>
+            </Grid>
+          </Grid>
         </form>
+        <Box sx={{ textAlign: "right" }}>
+          <Link
+            component={RouterLink}
+            to="/withdrawal"
+            variant="subtitle1"
+            sx={{
+              color: "#B5B5B5",
+              fontWeight: "500",
+              marginRight: "13px",
+              textDecoration: "none",
+              cursor: "pointer",
+            }}
+          >
+            회원탈퇴
+          </Link>
+        </Box>
       </Container>
     </div>
   );
