b65344e2e99b97296ba1271f9a2bcdd44a220dc2
Wlfjd <leejiwon813@naver.com>
2024-01-26 03:04:07 +0900

    ✨ Feat : CompantCard의  jdUrl 추가

--- src/components/common/card/CompanyCard.jsx
+++ src/components/common/card/CompanyCard.jsx
@@ -1,33 +1,43 @@
 import * as React from "react";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Box, CardActionArea, Paper } from "@mui/material";
+import { Box, CardActionArea, Link, Paper } from "@mui/material";
 
 import { CompanyCardStyle } from "./CardStyle";
 
 export default function CompanyCard({ data }) {
   return (
-    <Paper elevation={0} sx={{ my: 1 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={data.imageUrl}
-          sx={{ height: 200, borderRadius: "5px" }}
-        />
-        <Box sx={{ mt: 1 }}>
-          <Typography
-            variant="body3"
-            component="div"
-            color="#9A9AA1"
-            sx={CompanyCardStyle.Name}
-          >
-            {data.company}
-          </Typography>
-          <Typography color="#545459" fontSize="14px" fontWeight="500">
-            {data.title}
-          </Typography>
-        </Box>
-      </CardActionArea>
-    </Paper>
+    <Link
+      href={data.jdUrl}
+      style={{
+        textDecoration: "none",
+        background: "inherit !important",
+        outline: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Paper elevation={0} sx={{ my: 1 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={data.imageUrl}
+            sx={{ height: 200, borderRadius: "5px" }}
+          />
+          <Box sx={{ mt: 1 }}>
+            <Typography
+              variant="body3"
+              component="div"
+              color="#9A9AA1"
+              sx={CompanyCardStyle.Name}
+            >
+              {data.company}
+            </Typography>
+            <Typography color="#545459" fontSize="14px" fontWeight="500">
+              {data.title}
+            </Typography>
+          </Box>
+        </CardActionArea>
+      </Paper>
+    </Link>
   );
 }
