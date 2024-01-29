90af18493c55f881490ee4345426651eebf69272
Wlfjd <leejiwon813@naver.com>
2024-01-29 14:40:17 +0900

    ✨ Feat :  디코딩 함수 추가

--- src/pages/sign-in/KakaoRedirectPage.jsx
+++ src/pages/sign-in/KakaoRedirectPage.jsx
@@ -11,15 +11,23 @@ const KakaoRedirectPage = () => {
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    const value = searchParams.get("value");
-    const valueWithoutSpac = value.replace(/\s/g, "");
-    const hmac = searchParams.get("hmac");
-    const hmacWithoutSpace = hmac.replace(/\s/g, "");
-    const code = searchParams.get("code");
-    console.log(valueWithoutSpac, hmacWithoutSpace);
+    console.log(searchParams);
+    const decodeValue = (value) => {
+      try {
+        return decodeURIComponent(value);
+      } catch (error) {
+        console.error("Error decoding value:", error);
+        return null;
+      }
+    };
 
+    const value = decodeValue(searchParams.get("value"));
+    const hmac = decodeValue(searchParams.get("hmac"));
+    const code = searchParams.get("code");
+
+    console.log(value, hmac, code);
     if (hmac) {
-      navigate("/info"); // 이미 존재하는 경우 메인 페이지로 이동
+      // navigate("/info"); // 이미 존재하는 경우 메인 페이지로 이동
       setData((prev) => ({ ...prev, encrypted: value, hmac: hmac }));
     }
   }, [location, navigate]);
