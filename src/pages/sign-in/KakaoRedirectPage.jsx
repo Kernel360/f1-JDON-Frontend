6299acd017bd486d7858ae4baa185be0bc4f41a1
Wlfjd <leejiwon813@naver.com>
2024-01-29 15:08:35 +0900

    ✨ Feat :  카카오 로그인 성공

--- src/pages/sign-in/KakaoRedirectPage.jsx
+++ src/pages/sign-in/KakaoRedirectPage.jsx
@@ -12,26 +12,17 @@ const KakaoRedirectPage = () => {
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     console.log(searchParams);
-    const decodeValue = (value) => {
-      try {
-        return decodeURIComponent(value);
-      } catch (error) {
-        console.error("Error decoding value:", error);
-        return null;
-      }
-    };
-
-    const value = decodeValue(searchParams.get("value"));
-    const hmac = decodeValue(searchParams.get("hmac"));
+    const value = searchParams.get("value");
+    const hmac = searchParams.get("hmac");
     const code = searchParams.get("code");
-
     console.log(value, hmac, code);
     if (hmac) {
-      // navigate("/info"); // 이미 존재하는 경우 메인 페이지로 이동
+      navigate("/info"); // 이미 존재하는 경우 메인 페이지로 이동
       setData((prev) => ({ ...prev, encrypted: value, hmac: hmac }));
+    } else {
+      navigate("/"); // 새로운 인증 정보인 경우 정보 입력 페이지로 이동
     }
   }, [location, navigate]);
-  //   navigate("/signin"); // 새로운 인증 정보인 경우 정보 입력 페이지로 이동
   // userInfo에 이미 value와 hmac이 존재하는지 확인
 
   return (
