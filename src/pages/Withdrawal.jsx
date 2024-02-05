7f2d04cb9a960d16178cee22d3692c9ea7315360
jjomi <j.hist26@gmail.com>
2024-02-05 14:06:42 +0900

    [feat] 회원탈퇴 입력값 검증 및 버튼 활성화

--- src/pages/Withdrawal.jsx
+++ src/pages/Withdrawal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/common/Header";
 import InputField from "../components/common/InputField";
 import { Box, Button, Container } from "@mui/material";
@@ -6,21 +6,41 @@ import { buttonStyle } from "../components/common/navigation-btn/NavigationBtnSt
 import { useNavigate } from "react-router-dom";
 import { deleteMember } from "../api/api";
 
+// 이메일 유효성 검사 함수
+const isValidEmail = (email) => {
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  return emailRegex.test(email);
+};
+
 export default function Withdrawal() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleEmailChange = (value) => {
+    // 이메일 유효성 검사
+    if (value && !isValidEmail(value)) {
+      setEmailError("유효하지 않은 이메일 형식입니다.");
+    } else {
+      setEmailError("");
+    }
+    setEmail(value);
+    console.log("e1111ee", value);
+  };
 
   const handleSaveChanges = async () => {
     try {
+      console.log("최종Email", email);
       const res = await deleteMember();
       if (res) {
-        console.log(`${res}회원탈퇴합니다`);
-        //로그인 여부 상태값 변경
+        console.log(`${res || null}회원탈퇴합니다`);
+
+        localStorage.setItem("isLoggedInState", "false");
         navigate("/");
       }
     } catch (error) {
       console.error("Withdrawal파일 deleteMember 통신에러", error);
     }
-    // console.log("탈퇴합니다.");
   };
   return (
     <Container
@@ -28,8 +48,8 @@ export default function Withdrawal() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "85vh", // 전체 화면 높이로 설정
-        justifyContent: "space-between", // 상단, 하단 여백으로 요소 배치
+        minHeight: "85vh",
+        justifyContent: "space-between",
         paddingX: "29px",
       }}
     >
@@ -37,16 +57,24 @@ export default function Withdrawal() {
 
       <InputField
         label={"이메일"}
+        name={"email"}
         type={"email"}
         placeholder={"이메일을 입력해주세요"}
+        value={email}
+        onChange={(name, value) => handleEmailChange(value)}
+        error={emailError !== ""}
+        helperText={emailError}
+        onBlur={() => handleEmailChange(email)}
       />
 
       <Button
         onClick={handleSaveChanges}
+        disabled={emailError !== "" || email === ""}
+        fullWidth
+        mb={1}
         sx={{
           ...buttonStyle.Button,
-          width: "100%",
-          marginBottom: "10px",
+          ...(emailError !== "" || email == "" ? {} : buttonStyle.ActiveButton),
         }}
       >
         탈퇴
