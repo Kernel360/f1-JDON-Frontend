38b5befb3cde39f3bc515170b2f4201b3b416d74
Wlfjd <leejiwon813@naver.com>
2024-02-06 00:06:39 +0900

    ✨ Feat :  버그버튼, 모달 스타일 수정

--- src/pages/mainpage/Main.jsx
+++ src/pages/mainpage/Main.jsx
@@ -272,31 +272,30 @@ export function Main() {
 
       <VideoSection selectedChip={selectedChip} data={lectureList} />
       <CompanySection selectedChip={selectedChip} data={jdList} />
-      <div
+
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSfx9-ahkuQtQVy93P_nIhBbip-S4Q6RGnvqH1FeOA_Gu2F-Lg/viewform"
+        target="_blank"
+        rel="noopener noreferrer"
         style={{
-          background: theme.palette.primary.main,
-          height: 60,
+          display: "inline-block", // 블록 레벨 요소처럼 만들어 너비와 높이 적용
+          height: "60px",
           borderRadius: "10px",
+          width: "100%",
           textAlign: "center",
-          lineHeight: "60px",
+          lineHeight: "60px", // 텍스트를 버튼 중앙에 위치시킴
           margin: "15px auto",
+          background: theme.palette.primary.main, // theme가 정의되어 있어야 함
+          fontWeight: 600,
+          color: "white",
+          fontSize: "18px",
+          textDecoration: "none",
+          padding: "0 15px", // 좌우 패딩 적용,
         }}
       >
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfx9-ahkuQtQVy93P_nIhBbip-S4Q6RGnvqH1FeOA_Gu2F-Lg/viewform"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            width: "100%",
-            fontWeight: 600,
-            color: "white",
-            fontSize: "18px",
-            textDecoration: "none",
-          }}
-        >
-          버그 제출
-        </a>
-      </div>
+        버그 제출
+      </a>
+
       <BottomNav></BottomNav>
     </Container>
   );
