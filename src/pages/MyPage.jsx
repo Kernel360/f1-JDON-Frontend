84c21cf6b4ebd1e683c46fd5520072fc7179c23e
jjomi <j.hist26@gmail.com>
2024-02-06 19:51:46 +0900

    [fix] api 통신에러로인한 임시 막은거 활성화함

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -201,10 +201,11 @@ export default function MyPage() {
         const memberData = await getMemberInfo();
         const faqData = await getFAQ();
 
-        setMemberInfo(memberData.data);
+        // if(memberData)
+        setMemberInfo(memberData.data.response.status);
         setFAQ(faqData.faqList || []);
-        // console.log("memberData", memberData.data.nickname);
-        // console.log("faq", faqData.faqList);
+        // // console.log("memberData", memberData.data.nickname);
+        // // console.log("faq", faqData.faqList);
       } catch (error) {
         if (error.response && error.response.status === 401) {
           navigator("/");
@@ -217,16 +218,16 @@ export default function MyPage() {
 
   const handleLogout = async () => {
     try {
-      // const res = await logoutMember();
-      // console.log(res);
+      const res = await logoutMember();
+      console.log(res);
 
-      localStorage.setItem("isLoggedInState", "false");
-      navigate("/");
+      // localStorage.setItem("isLoggedInState", "false");
+      // navigate("/");
 
-      //   if (res === 302) {
-      //     localStorage.setItem("isLoggedInState", "false");
-      //     navigate("/");
-      //   }
+      if (res === 302) {
+        localStorage.setItem("isLoggedInState", "false");
+        navigate("/");
+      }
     } catch (error) {
       console.error("로그아웃 에러", error);
     }
