6be7e82a9e00ed1bf8edb61d7a5bd4823e77ccf0
jjomi <j.hist26@gmail.com>
2024-02-07 18:11:53 +0900

    [fix] 마이페이지 영상찜 화면 페이지 0으로 픽스

--- src/pages/FavoritesVideo.jsx
+++ src/pages/FavoritesVideo.jsx
@@ -10,7 +10,7 @@ import Pagenation from "../components/common/Pagenation";
 
 export default function FavoritesVideo() {
   const [datas, setDatas] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const [page, setPage] = useState({});
 
   console.log("datas", datas);
