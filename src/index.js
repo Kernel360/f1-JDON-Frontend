7de19eea9dcec38936410949e6cc0a70ce5a49bf
jjomi <j.hist26@gmail.com>
2024-02-16 11:14:00 +0900

    [fix] index.js 파일에서 httpsRedirect 삭제

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App.jsx";
-import HttpsRedirect from "react-https-redirect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HttpsRedirect>
-    <App />
-  </HttpsRedirect>
-);
+root.render(<App />);
 reportWebVitals();
