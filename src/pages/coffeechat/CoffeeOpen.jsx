198184f538b981d0d0f02f23555304eda6cc7452
Wlfjd <leejiwon813@naver.com>
2024-02-23 00:15:59 +0900

    ♻️ Refactor :   && 연산자 로 조건문 수정

--- src/pages/coffeechat/CoffeeOpen.jsx
+++ src/pages/coffeechat/CoffeeOpen.jsx
@@ -38,13 +38,9 @@ function Coffeeopen() {
     try {
       await registerCoffeeChat(formValue);
       setIsRegistered(true);
-      if (
-        window.confirm(
-          "신청이 완료되었습니다. 커피챗 신청 내역을 확인하시겠습니까?"
-        )
-      ) {
-        navigate("/mypage");
-      }
+      window.confirm(
+        "신청이 완료되었습니다. 커피챗 신청 내역을 확인하시겠습니까?"
+      ) && navigate("/mypage");
     } catch (error) {
       console.error("Error registering coffee chat:", error);
     }
--- src/pages/mypage/FavoritesVideo.jsx
+++ src/pages/mypage/FavoritesVideo.jsx
@@ -1,88 +1,78 @@
 import React, { useState, useEffect } from "react";
-import { Box, Typography, Grid, Container } from "@mui/material";
+import { Container, Box, Typography, Grid } from "@mui/material";
+import BottomNav from "../../components/common/BottomNav";
+import VideoCard from "../../components/common/card/VideoCard";
 import { getFavoritVideo } from "../../api/api";
 import Header from "../../components/common/Header";
-import VideoCard from "../../components/common/card/VideoCard";
-import BottomNav from "../../components/common/BottomNav";
 import Pagenation from "../../components/common/Pagenation";
 
 export default function FavoritesVideo() {
   const [datas, setDatas] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
   const [page, setPage] = useState({});
-  const [isFavoriteChanged, setIsFavoriteChanged] = useState(false);
 
-  console.log("datas", page);
+  console.log("datas", datas);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await getFavoritVideo();
         setDatas(res.data.content);
         setPage(res.data.pageInfo);
+        // console.log("찜 확인11", res);
+        // console.log("datas", datas);
       } catch (error) {
         console.error("getFavoritVideo API 에러", error);
       }
     };
-
-    if (!datas || isFavoriteChanged) {
+    // 최초 렌더링 시에만 fetchData 호출
+    if (!datas) {
       fetchData();
-      setIsFavoriteChanged(false); // isFavoriteChanged 상태 초기화
     }
-  }, [currentPage, datas, isFavoriteChanged]);
+    // fetchData();
+  }, [currentPage, datas]);
 
   const handlePageChange = (event, value) => {
     console.log(`현재 페이지: ${value}`);
     setCurrentPage(value);
   };
 
-  const handleFavoriteChange = () => {
-    // 추가: isFavorite 상태가 변경될 때 호출되는 함수
-    setIsFavoriteChanged(true);
-  };
-
   return (
     <Container
       maxWidth="md"
-      paddingX={"16px"}
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "100vh",
+        minHeight: "95vh",
+        minwidth: "100vw",
+        pb: 10,
       }}
     >
       <Header title={"찜"} />
-      <Box mt={2}>
-        <Grid container spacing={{ xs: 2, md: 2 }} sx={{ py: 1 }}>
-          {datas && datas.length > 0 ? (
-            datas.map((item, index) => (
-              <Grid item xs={12} sm={4} md={4} key={index}>
-                <VideoCard
-                  data={item}
-                  myFavorite={true}
-                  onSuccess={handleFavoriteChange}
-                />
-              </Grid>
-            ))
-          ) : (
-            <Box mt={9} sx={{ width: "100%" }}>
-              <Typography
-                variant="h6"
-                color="textSecondary"
-                sx={{
-                  textAlign: "center",
-                  fontSize: 16,
-                  mt: 3,
-                }}
-              >
-                찜한 영상이 없습니다!
-              </Typography>
-            </Box>
-          )}
-        </Grid>
-      </Box>
-      <Box sx={{ flexGrow: 1 }} />
+      <Grid container spacing={{ xs: 2, md: 2 }} sx={{ px: 2, py: 1 }}>
+        {datas && datas.length > 0 ? (
+          datas.map((item, index) => (
+            <Grid item xs={12} sm={4} md={4} key={index}>
+              <VideoCard data={item} />
+            </Grid>
+          ))
+        ) : (
+          <Box mt={9} sx={{ width: "100%" }}>
+            <Typography
+              variant="h6"
+              color="textSecondary"
+              sx={{
+                textAlign: "center",
+                fontSize: 16,
+                mt: 3,
+              }}
+            >
+              찜한 영상이 없습니다!
+            </Typography>
+          </Box>
+        )}
+      </Grid>
       {datas && (
-        <Box mb={11}>
+        <Box mt={4}>
           <Pagenation
             pageCount={page?.totalPages}
             currentPage={currentPage}
@@ -90,7 +80,7 @@ export default function FavoritesVideo() {
           />
         </Box>
       )}
-      <BottomNav />
+      <BottomNav></BottomNav>
     </Container>
   );
 }
