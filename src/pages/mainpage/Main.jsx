fa493fd1b1fb3bc6463d717d63d435f695cb2e59
Wlfjd <leejiwon813@naver.com>
2024-02-22 16:08:09 +0900

    ♻️ Refactor :  Main.jsx 기능별로 컴포넌트 분리 및 isLogin prop으로 내려주기

--- src/pages/mainpage/Main.jsx
+++ src/pages/mainpage/Main.jsx
@@ -1,68 +1,30 @@
-import { Container } from '@mui/material';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import BottomNav from '../../components/common/BottomNav';
-import CompanySection from './CompanySection';
-import VideoSection from './VideoSection';
-import HeaderWithSearchBar from './HeaderWithSearchBar';
-import SubmitBug from './SubmitBug';
-import StickyTabSection from './StickyTabSection';
-import { fetchLectureData, fetchUserInfo } from './apiFunction';
-import SrcollToTop from './ScrollToTop';
-import { Authentication } from '../../api/api';
-import { useRecoilState } from 'recoil';
-import { isLoggedInState } from '../../recoil/atoms';
+import { Container } from "@mui/material";
+import { useEffect, useRef } from "react";
+import BottomNav from "../../components/common/BottomNav";
+import CompanySection from "./CompanySection";
+import VideoSection from "./VideoSection";
+import HeaderWithSearchBar from "./HeaderWithSearchBar";
+import SubmitBug from "./SubmitBug";
+import StickyTabSection from "./StickyTabSection";
+import SrcollToTop from "./ScrollToTop";
+import { useAuth } from "./useAuth";
+import { useLoadData } from "./useLoadData";
 
 export function Main() {
-  const [isLogin, setIsLogin] = useRecoilState(isLoggedInState);
-  const [selectedChip, setSelectedChip] = useState({});
-  const [lectureList, setLectureList] = useState([]);
-  const [isInitialLoad, setIsInitialLoad] = useState(true);
-  const [jdList, setJdList] = useState([]);
+  const isLogin = useAuth(); // 로그인 상태 관리
+  const { selectedChip, setSelectedChip, lectureList, jdList } = useLoadData(); // 데이터 로딩 및 상태 관리
   const topRef = useRef(null);
-
-  const loadData = useCallback(
-    async (keyword, userSelected) => {
-      const lectureData = await fetchLectureData(keyword);
-      setLectureList(lectureData.lectureList);
-      setJdList(lectureData.jdList);
-      if (isInitialLoad) {
-        setSelectedChip({ keyword: lectureData.keyword, userSelected });
-        setIsInitialLoad(false);
-      }
-    },
-    [isInitialLoad]
-  );
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const state = await Authentication();
-      setIsLogin(state);
-    };
-    fetchData();
-  }, [setIsLogin]);
-  console.log('호출하는중', isLogin);
-
-  useEffect(() => {
-    if (isInitialLoad) {
-      loadData('', false);
-      fetchUserInfo();
-      setIsInitialLoad(false);
-    }
-  }, [isInitialLoad, loadData]);
-
-  useEffect(() => {
-    if (selectedChip.userSelected) {
-      loadData(selectedChip.keyword);
-      setIsInitialLoad(false);
-    }
-  }, [selectedChip, loadData]);
 
   return (
     <div ref={topRef}>
-      <Container maxWidth="md" sx={{ pb: 10, position: 'relative' }}>
+      <Container maxWidth="md" sx={{ pb: 10, position: "relative" }}>
         <HeaderWithSearchBar setSelectedChip={setSelectedChip} />
         <SrcollToTop topRef={topRef} />
-        <StickyTabSection selectedChip={selectedChip} setSelectedChip={setSelectedChip} />
+        <StickyTabSection
+          isLogin={isLogin}
+          selectedChip={selectedChip}
+          setSelectedChip={setSelectedChip}
+        />
         <VideoSection selectedChip={selectedChip} data={lectureList} />
         <CompanySection selectedChip={selectedChip} data={jdList} />
         <SubmitBug />
