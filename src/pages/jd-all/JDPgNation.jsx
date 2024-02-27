0ee94731285f4689d94643e13e36b55ec27727b0
junkue20 <junkue13@gmail.com>
2024-02-27 19:09:56 +0900

    🐛 Fix : 페이지네이션 컴포넌트 오류 수정

--- src/pages/jd-all/JDPgNation.jsx
+++ src/pages/jd-all/JDPgNation.jsx
@@ -1,21 +1,17 @@
-import { Stack } from "@mui/material";
-import React, { useState } from "react";
-import PaginationComponent from "../../components/common/Pagenation";
+import { Stack } from '@mui/material';
+import React, { useState } from 'react';
+import PaginationComponent from '../../components/common/Pagenation';
 
 function JDPgNation() {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const handlePageChange = (newPage) => {
+  const handlePageChange = (_, newPage) => {
     setCurrentPage(newPage);
   };
-  
+
   return (
     <Stack justifyContent="center" alignItems="center" sx={{ mb: 12 }}>
-      <PaginationComponent
-        pageCount={10}
-        currentPage={currentPage}
-        onChange={handlePageChange}
-      />
+      <PaginationComponent pageCount={10} currentPage={currentPage} onChange={handlePageChange} />
     </Stack>
   );
 }
