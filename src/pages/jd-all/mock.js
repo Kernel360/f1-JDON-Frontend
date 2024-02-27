d89ffdacb12db2e11d529e323ac2da684445f665
junkue20 <junkue13@gmail.com>
2024-02-27 19:08:46 +0900

    📦 Chore : 목업 데이터 갯수 조절

--- src/pages/jd-all/mock.js
+++ src/pages/jd-all/mock.js
@@ -1,114 +1,62 @@
 export const SAMPLE_DATA = [
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-    {
-      id: "example_id_value",
-      imageUrl:
-        "https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg",
-      company: "(주)JDON",
-      title: "신입 개발자를 구합니다",
-    },
-  ];
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+  {
+    id: 'example_id_value',
+    imageUrl: 'https://img.freepik.com/free-photo/low-angle-view-of-skyscrapers_1359-1105.jpg',
+    company: '(주)JDON',
+    title: '신입 개발자를 구합니다',
+  },
+];
