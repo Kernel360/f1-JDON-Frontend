4cfa6020412e807dff260550f36199332e3561e2
Wlfjd <leejiwon813@naver.com>
2024-02-27 13:34:15 +0900

    ✨ Feat :  찜 성공/취소 alert 없애기

--- src/pages/mainpage/VideoSection.jsx
+++ src/pages/mainpage/VideoSection.jsx
@@ -4,11 +4,6 @@ import VideoCard from "../../components/common/card/VideoCard";
 import hot from "../../../src/assets/images/hot.svg";
 
 function VideoSection({ selectedChip, data }) {
-  const handleSuccess = (isFavorite) => {
-    if (isFavorite) alert("찜 취소");
-    else alert("찜 성공");
-  };
-
   const handleError = (error) => {
     alert(`오류 발생: ${error.message}`);
   };
@@ -42,11 +37,7 @@ function VideoSection({ selectedChip, data }) {
             <Grid container spacing={{ xs: 2, md: 2 }} sx={{ px: 2, py: 1 }}>
               {data.map((video, index) => (
                 <Grid item xs={12} sm={4} md={4} key={video.lectureId}>
-                  <VideoCard
-                    data={video}
-                    onSuccess={handleSuccess}
-                    onError={handleError}
-                  />
+                  <VideoCard data={video} onError={handleError} />
                 </Grid>
               ))}
             </Grid>
