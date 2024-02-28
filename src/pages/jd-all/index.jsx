7f66ac9f6c5d98a9077a423cd52820344d6f53f3
junkue20 <junkue13@gmail.com>
2024-02-28 19:37:49 +0900

    ♻️ Refactor : 컴포넌트 재 분류

--- src/pages/jd-all/index.jsx
+++ src/pages/jd-all/index.jsx
@@ -1,21 +1,18 @@
-import React from "react";
-import { Container } from "@mui/material";
-import BottomNav from "../../components/common/BottomNav";
-import JDSearchBar from "./JDSearchBar";
-import JDComponent from "./JDComponent";
-import JDPageNation from "./JDPgNation";
-import CommonHeader from "../../components/common/Header";
+import React from 'react';
+import { Container } from '@mui/material';
+import BottomNav from '../../components/common/BottomNav';
+import JDSearchBar from './JDSearchBar';
+import JDComponent from './JDComponent';
+import CommonHeader from '../../components/common/Header';
 function JdAll() {
   return (
     <Container maxWidth="md">
       {/* 헤더 (뒤로가기) */}
-      <CommonHeader title={'메인 페이지'}/>
+      <CommonHeader title={'메인 페이지'} />
       {/* 상단 검색바 */}
       <JDSearchBar />
-      {/* 공고 내역 컴포넌트 */}
+      {/* 공고 내역 컴포넌트 & 페이지네이션 */}
       <JDComponent />
-      {/* 페이지네이션 컴포넌트 (추후 삭제되거나 수정될듯함!)*/}
-      <JDPageNation />
       {/* 하단 Navbar */}
       <BottomNav />
     </Container>
