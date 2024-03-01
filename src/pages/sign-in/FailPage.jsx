4e7dceaac8e9f265200f462c13dea4649b1ec7e7
junkue20 <junkue13@gmail.com>
2024-03-02 01:40:29 +0900

    📦 Chore : not-found-email 에러 문구 수정

--- src/pages/sign-in/FailPage.jsx
+++ src/pages/sign-in/FailPage.jsx
@@ -1,34 +1,32 @@
-import { Box, Button, Container, Typography } from "@mui/material";
-import { infoBasicStyles } from "../info/InfoStyles";
-import { buttonStyle } from "../../components/common/navigation-btn/NavigationBtnStyles";
-import { useNavigate } from "react-router-dom";
-import { Close } from "@mui/icons-material";
+import { Box, Button, Container, Typography } from '@mui/material';
+import { infoBasicStyles } from '../info/InfoStyles';
+import { buttonStyle } from '../../components/common/navigation-btn/NavigationBtnStyles';
+import { useNavigate } from 'react-router-dom';
+import { Close } from '@mui/icons-material';
 
 export function FailPage() {
   const navigate = useNavigate();
   return (
     <Container
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
       }}
     >
-      <Typography sx={{ fontSize: "25px", fontWeight: 500, p: 4 }}>
-        로그인 실패
-      </Typography>
+      <Typography sx={{ fontSize: '25px', fontWeight: 500, p: 4 }}>로그인 실패</Typography>
       <Typography sx={infoBasicStyles.typographySubtitle}>
-        사용자의 이메일이 등록되지 않았거나 정보 매칭에 실패하였습니다
+        해당 소셜 서비스에 이메일을 등록해야 JDON 서비스를 이용하실 수 있습니다.
       </Typography>
       <Box sx={{ padding: 4 }}>
-        <Close sx={{ fontSize: "40px" }} />
+        <Close sx={{ fontSize: '40px' }} />
       </Box>
       <Button
         sx={buttonStyle.EndButton}
         onClick={() => {
-          navigate("/signin");
+          navigate('/signin');
         }}
       >
         로그인 재시도
