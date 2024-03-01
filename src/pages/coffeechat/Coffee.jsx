b2302ad29ecf0e30296ec7910e27ceac352d8bcc
junkue20 <junkue13@gmail.com>
2024-03-02 01:32:10 +0900

    ♻️ Refactor : 상대경로 -> 절대경로로 수정 및 QA 사항 반영 (헤더 통일)

--- src/pages/coffeechat/Coffee.jsx
+++ src/pages/coffeechat/Coffee.jsx
@@ -1,13 +1,14 @@
-import { Box, Container, Grid, Stack, Typography } from "@mui/material";
-import BottomNav from "../../components/common/BottomNav";
-import CoffeeChatCard from "../../components/common/card/CoffeeChatCard";
-import { useEffect, useState } from "react";
-import { getCoffeeChat, getJobCategory } from "../../api/api";
-import PaginationComponent from "../../components/common/Pagenation";
-import CoffeeBanner from "./CoffeeBanner";
-import FiltersAndButton from "./FiltersAndButton";
-import { useRecoilState } from "recoil";
-import { kindOfJdState } from "../../recoil/atoms";
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import BottomNav from 'components/common/BottomNav';
+import CoffeeChatCard from 'components/common/card/CoffeeChatCard';
+import { useEffect, useState } from 'react';
+import { getCoffeeChat, getJobCategory } from 'api/api';
+import PaginationComponent from 'components/common/Pagenation';
+// import CoffeeBanner from './CoffeeBanner';
+import FiltersAndButton from './FiltersAndButton';
+import { useRecoilState } from 'recoil';
+import { kindOfJdState } from 'recoil/atoms';
+import HeaderWithSearchBar from 'components/common/search-bar/HeaderWithSearchBar';
 
 export function Coffee() {
   const [coffeeData, setCoffeeData] = useState({
@@ -21,8 +22,8 @@ export function Coffee() {
     },
   });
   const [sortData, setSortData] = useState({
-    sorting: "createdDate",
-    jobCategory: "",
+    sorting: 'createdDate',
+    jobCategory: '',
   });
   const [currentPage, setCurrentPage] = useState(1);
   const [kindOfJd, setKindOfJd] = useRecoilState(kindOfJdState);
@@ -42,10 +43,10 @@ export function Coffee() {
         );
         setCoffeeData(data.data.data);
       } catch (error) {
-        console.error("Error fetching getCoffeeChat:", error);
+        console.error('Error fetching getCoffeeChat:', error);
       }
     })(currentPage);
-  }, [sortData.sorting, sortData.jobCategory, currentPage]);
+  }, [sortData.sorting, sortData.jobCategory, currentPage, coffeeData.pageInfo.pageSize]);
 
   useEffect(() => {
     (async () => {
@@ -53,19 +54,16 @@ export function Coffee() {
         const { jobGroupList } = await getJobCategory();
         setKindOfJd(jobGroupList[0].jobCategoryList);
       } catch (error) {
-        console.error("Error fetching job categories:", error);
+        console.error('Error fetching job categories:', error);
       }
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Container maxWidth="md" sx={{ pt: 3, pb: 10 }}>
-      <CoffeeBanner />
-      <FiltersAndButton
-        sortData={sortData}
-        onChange={setSortData}
-        kindOfJd={kindOfJd}
-      />
+    <Container maxWidth="md" sx={{ pb: 10 }}>
+      <HeaderWithSearchBar isSearchBarTrue={false} />
+      <FiltersAndButton sortData={sortData} onChange={setSortData} kindOfJd={kindOfJd} />
       <Grid container spacing={{ xs: 2, md: 2 }}>
         {coffeeData?.content?.length > 0 ? (
           coffeeData.content.map((item, index) => (
@@ -78,10 +76,10 @@ export function Coffee() {
             sx={{
               ml: 2,
               mt: 8,
-              width: "100%",
-              textAlign: "center",
-              fontSize: "16px",
-              color: "#B9B9B9",
+              width: '100%',
+              textAlign: 'center',
+              fontSize: '16px',
+              color: '#B9B9B9',
               fontWeight: 600,
             }}
           >
@@ -90,11 +88,7 @@ export function Coffee() {
         )}
       </Grid>
       {coffeeData?.content?.length > 0 && (
-        <BasicPagination
-          coffeeData={coffeeData}
-          currentPage={currentPage}
-          handlePageChange={handlePageChange}
-        />
+        <BasicPagination coffeeData={coffeeData} currentPage={currentPage} handlePageChange={handlePageChange} />
       )}
       <BottomNav />
     </Container>
@@ -105,7 +99,7 @@ function BasicPagination({ coffeeData, currentPage, handlePageChange }) {
   return (
     <Box
       sx={{
-        width: "100%",
+        width: '100%',
         py: 3,
       }}
     >
