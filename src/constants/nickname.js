b521942b92ba705982013f5e702a69f46cc315a9
junkue20 <junkue13@gmail.com>
2024-03-03 15:27:09 +0900

    ♻️ Refactor : 유효성 검사 상수 변수명 UPPER_CASE로 변경

--- src/constants/nickname.js
+++ src/constants/nickname.js
@@ -1,2 +1,4 @@
-export const NoSCAndAdmin = /[\{\}\[\]\/?.,;:|\(\)*~`!^\-_+<>@#$%&=\'\"\_]|관리자|어드민|admin|administer/g;
-export const NoSpaceBar = /\s/g;
+// eslint-disable-next-line
+export const NO_SC = /[\{\}\[\]\/?.,;:|\(\)*~`!^\-_+<>@#$%&=\'\"\_]/g;
+export const NO_ADMIN = /관리자|어드민|admin|administer/g;
+export const NO_SPACE_BAR = /\s/g;
