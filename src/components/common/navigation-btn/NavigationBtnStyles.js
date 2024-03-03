ce6d3a74bf309674d1888eb3cf3b106ea164301b
junkue20 <junkue13@gmail.com>
2024-03-03 15:26:24 +0900

    🐛 Fix : 404page 버튼 hover효과 삭제

--- src/components/common/navigation-btn/NavigationBtnStyles.js
+++ src/components/common/navigation-btn/NavigationBtnStyles.js
@@ -1,37 +1,41 @@
 export const buttonStyle = {
   Container: {
-    position: "absolute", // 화면 중앙에 위치하기 위해 'absolute' 사용
+    position: 'absolute', // 화면 중앙에 위치하기 위해 'absolute' 사용
     bottom: 10, // 화면 하단에서의 위치
-    left: "50%", // 화면 중앙에 위치
-    background: "white",
-    transform: "translateX(-50%)", // 정확한 중앙 정렬을 위한 변환
+    left: '50%', // 화면 중앙에 위치
+    background: 'white',
+    transform: 'translateX(-50%)', // 정확한 중앙 정렬을 위한 변환
   },
 
   Button: {
     mt: 5,
     mb: 2,
-    p: "13px",
-    borderRadius: "999px",
-    background: "#EBEBEB",
-    color: "#BCBCC4",
-    fontSize: "16px",
-    "&:hover": {
-      color: "#ffffff",
-      backgroundColor: "#6482FF", // 클릭(마우스 오버) 시 배경색 변경
+    p: '13px',
+    borderRadius: '999px',
+    background: '#EBEBEB',
+    color: '#BCBCC4',
+    fontSize: '16px',
+    '&:hover': {
+      color: '#ffffff',
+      backgroundColor: '#6482FF', // 클릭(마우스 오버) 시 배경색 변경
     },
   },
   ActiveButton: {
-    color: "#ffffff",
-    backgroundColor: "#6482FF",
+    color: '#ffffff',
+    backgroundColor: '#6482FF',
   },
   EndButton: {
     mt: 7,
     mb: 2,
-    width: "60%",
-    p: "13px",
-    borderRadius: "999px",
-    background: "#6482FF",
-    color: "#ffffff",
-    fontSize: "16px",
+    width: '60%',
+    p: '13px',
+    borderRadius: '999px',
+    background: '#6482FF',
+    color: '#ffffff',
+    fontSize: '16px',
+    '&:hover': {
+      color: '#ffffff',
+      backgroundColor: '#6482FF',
+    },
   },
 };
