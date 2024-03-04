1e29350b09b7ae70b1ee3eaa18be42bf031f7f1b
Wlfjd <leejiwon813@naver.com>
2024-03-04 20:14:12 +0900

    🐛 Fix :  리뷰 api 호출할 때 로그인 유무 확인해서 분기타는거로 변경

--- src/pages/jd-detail/useReviewPagination.jsx
+++ src/pages/jd-detail/useReviewPagination.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
 import { getReivew } from "../../api/api";
 import { useInView } from "react-intersection-observer";
+import { useRecoilValue } from "recoil";
+import { isLoggedInState } from "recoil/atoms";
+import { useNavigate } from "react-router-dom";
 
 export const useReviewPagination = (id) => {
+  const navigate = useNavigate();
   const [page, setPage] = useState(0);
   const [lastPage, setLastPage] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [reviewData, setReviewData] = useState([]);
   const [ref, inView] = useInView();
+  const isLogin = useRecoilValue(isLoggedInState).isLoginUser;
 
   const fetchReviewData = async (reset = false) => {
+    if (!isLogin) {
+      promptLogin();
+      return;
+    }
     if (reset) {
       setPage(0);
       setReviewData([]);
@@ -29,6 +38,15 @@ export const useReviewPagination = (id) => {
       console.log(e);
     }
     setIsLoading(false);
+  };
+
+  const promptLogin = () => {
+    const confirmResult = window.confirm(
+      "리뷰 작성은 로그인 후 이용할 수 있습니다. 로그인 하시겠습니까?"
+    );
+    if (confirmResult) {
+      navigate("/signin");
+    }
   };
 
   useEffect(() => {
