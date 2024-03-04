dc1d61fb37dd3aaa65a5326d5c2ff32f41911041
junkue20 <junkue13@gmail.com>
2024-03-04 22:30:15 +0900

    🐛 Fix : JD 상세페이지 로그인 alert 수정

--- src/pages/jd-detail/useReviewPagination.jsx
+++ src/pages/jd-detail/useReviewPagination.jsx
@@ -1,24 +1,24 @@
 import { useState, useEffect } from 'react';
 import { getReivew } from '../../api/api';
 import { useInView } from 'react-intersection-observer';
-import { useRecoilValue } from 'recoil';
-import { isLoggedInState } from 'recoil/atoms';
-import { useNavigate } from 'react-router-dom';
+// import { useRecoilValue } from 'recoil';
+// import { isLoggedInState } from 'recoil/atoms';
+// import { useNavigate } from 'react-router-dom';
 
 export const useReviewPagination = (id) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [page, setPage] = useState(0);
   const [lastPage, setLastPage] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [reviewData, setReviewData] = useState([]);
   const [ref, inView] = useInView();
-  const isLogin = useRecoilValue(isLoggedInState).isLoginUser;
+  // const isLogin = useRecoilValue(isLoggedInState).isLoginUser;
 
   const fetchReviewData = async (reset = false) => {
-    if (!isLogin) {
-      promptLogin();
-      return;
-    }
+    // if (!isLogin) {
+    //   promptLogin();
+    //   return;
+    // }
     if (reset) {
       setPage(0);
       setReviewData([]);
@@ -41,10 +41,10 @@ export const useReviewPagination = (id) => {
   };
 
   const promptLogin = () => {
-    const confirmResult = window.confirm('리뷰는 로그인 후 조회할 수 있습니다. 로그인 하시겠습니까?');
-    if (confirmResult) {
-      navigate('/signin');
-    }
+    // const confirmResult = window.confirm('리뷰는 로그인 후 조회할 수 있습니다. 로그인 하시겠습니까?');
+    // if (confirmResult) {
+    //   navigate('/signin');
+    // }
   };
 
   useEffect(() => {
