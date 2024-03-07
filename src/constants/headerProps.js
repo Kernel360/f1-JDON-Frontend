776ce1b0d2789a927b5f6c64adff113e9f1d2e51
junkue20 <junkue17@naver.com>
2024-03-07 19:28:14 +0900

    ✨ Feat : 공통 헤더 컴포넌트 버튼 제목 및 주소 상수 추가

--- src/constants/headerProps.js
+++ src/constants/headerProps.js
@@ -5,13 +5,22 @@
 
 // 커피챗 상세페이지 및 커피챗 생성 페이지
 export const COFFEE_CHILD = {
-    title: '커피챗 목록으로',
+    title: '커피챗 목록',
     url: '/coffee',
 }
-
+// 커피챗 - 수정화면에서 상세페이지로 이동하도록.
+export const COFFEE_CHILD_ID = {
+    title: '커피챗 상세페이지',
+    url : '/coffee/',
+}
+// 커피챗 - 마이페이지에서 타고 들어갈 시 다시 마이페이지로 오도록. (아직 적용 안됨.)
+export const COFFEE_MYPAGE_CHILD = {
+    title: '내 커피챗',
+    url: '/mypage/coffee',
+}
 // JD 상세 페이지
 export const JD_CHILD= {
-    title: 'JD-ON 목록으로',
+    title: 'JD-ON 목록',
     url: '/jd',
 }
 
@@ -21,8 +30,11 @@ export const MYPAGE_CHILD = {
     url: '/mypage',
 }
 
+
 // 회원탈퇴 페이지 
 export const USER_QUIT = {
     title: '내 정보 수정',
     url: '/mypage/infoedit',
 }
+
+
