d8e5bee7b66cc8404537a4222d72075b59ee6b03
junkue20 <junkue17@naver.com>
2024-03-07 18:19:35 +0900

    ♻️ Refactor : 각 페이지별 공통 헤더 버튼 및 뒤로가기 경로 상수파일 지정

---
+++ src/constants/headerProps.js
@@ -0,0 +1,28 @@
+
+/**
+ * Header 공동 컴포넌트에 있는 버튼의 제목 및 라우팅 경로를 지정해주는 파일입니다.  
+ * */
+
+// 커피챗 상세페이지 및 커피챗 생성 페이지
+export const COFFEE_CHILD = {
+    title: '커피챗 목록으로',
+    url: '/coffee',
+}
+
+// JD 상세 페이지
+export const JD_CHILD= {
+    title: 'JD-ON 목록으로',
+    url: '/jd',
+}
+
+// 마이페이지 (찜, 커피챗, 정보수정)
+export const MYPAGE_CHILD = {
+    title: '마이페이지',
+    url: '/mypage',
+}
+
+// 회원탈퇴 페이지 
+export const USER_QUIT = {
+    title: '내 정보 수정',
+    url: '/mypage/infoedit',
+}
