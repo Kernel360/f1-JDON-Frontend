03bf47954369fdb4de2ab7685b355811dabc3ae2
junkue20 <junkue13@gmail.com>
2024-03-12 02:11:30 +0900

    🐛 Fix : AWS 배포 관련 의존성 배열 이슈 해결

--- src/pages/jd-detail/useReviewPagination.jsx
+++ src/pages/jd-detail/useReviewPagination.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getReivew } from 'api/api';
 import { useInView } from 'react-intersection-observer';
 
@@ -9,36 +9,39 @@ export const useReviewPagination = (id) => {
   const [reviewData, setReviewData] = useState([]);
   const [ref, inView] = useInView();
 
-  const fetchReviewData = async (reset = false) => {
-    if (reset) {
-      setReviewId('');
-      setReviewData([]);
-      setLastPage(false);
-    }
-    if (isLoading || lastPage) return;
-    setIsLoading(true);
+  const fetchReviewData = useCallback(
+    async (reset = false) => {
+      if (reset) {
+        setReviewId('');
+        setReviewData([]);
+        setLastPage(false);
+      }
+      if (isLoading || lastPage) return;
+      setIsLoading(true);
 
-    try {
-      const res = await getReivew(id, reviewId);
-      setReviewData((prev) => [...prev, ...res.content]);
-      if (res.content.length > 0) {
-        const newReviewId = res.content[res.content.length - 1].id;
-        setReviewId(newReviewId);
+      try {
+        const res = await getReivew(id, reviewId);
+        setReviewData((prev) => [...prev, ...res.content]);
+        if (res.content.length > 0) {
+          const newReviewId = res.content[res.content.length - 1].id;
+          setReviewId(newReviewId);
+        }
+        if (res.pageInfo.last) {
+          setLastPage(true);
+        }
+      } catch (e) {
+        console.error(e);
       }
-      if (res.pageInfo.last) {
-        setLastPage(true);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    [id, reviewId, isLoading, lastPage]
+  );
 
   useEffect(() => {
     if ((inView && !isLoading) || reviewId) {
       fetchReviewData();
     }
-  }, [inView, reviewId, isLoading]);
+  }, [inView, reviewId, isLoading, fetchReviewData]);
 
   return {
     isLoading,
--- src/pages/mainpage/useLoadData.jsx
+++ src/pages/mainpage/useLoadData.jsx
@@ -1,34 +1,37 @@
-import { useEffect, useState } from "react";
-import { fetchLectureData } from "./apiFunction";
+import { useCallback, useEffect, useState } from 'react';
+import { fetchLectureData } from './apiFunction';
 
 export function useLoadData() {
   const [selectedChip, setSelectedChip] = useState({
-    keyword: "",
+    keyword: '',
     userSelected: false,
   });
   const [lectureList, setLectureList] = useState([]);
   const [jdList, setJdList] = useState([]);
   const [isInitialLoad, setIsInitialLoad] = useState(true);
 
-  const loadData = async (keyword, userSelected) => {
-    const res = await fetchLectureData(encodeURIComponent(keyword));
-    setLectureList(res.lectureList);
-    setJdList(res.jdList);
+  const loadData = useCallback(
+    async (keyword, userSelected) => {
+      const res = await fetchLectureData(encodeURIComponent(keyword));
+      setLectureList(res.lectureList);
+      setJdList(res.jdList);
 
-    if (isInitialLoad) {
-      setSelectedChip({ keyword: res.keyword, userSelected });
-      setIsInitialLoad(false);
-    }
-  };
+      if (isInitialLoad) {
+        setSelectedChip({ keyword: res.keyword, userSelected });
+        setIsInitialLoad(false);
+      }
+    },
+    [isInitialLoad]
+  );
 
   useEffect(() => {
-    isInitialLoad && loadData("", false);
+    isInitialLoad && loadData('', false);
     if (selectedChip.userSelected && selectedChip.keyword) {
       loadData(selectedChip.keyword, true);
     } else if (!selectedChip.keyword) {
       setIsInitialLoad(true);
     }
-  }, [isInitialLoad, selectedChip]);
+  }, [isInitialLoad, selectedChip, loadData]);
 
   return { selectedChip, setSelectedChip, lectureList, jdList };
 }
