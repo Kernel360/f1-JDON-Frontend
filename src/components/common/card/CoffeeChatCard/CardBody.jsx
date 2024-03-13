582a7363e29e42787df946df07d55770744ee7fd
Wlfjd <leejiwon813@naver.com>
2024-03-13 12:14:53 +0900

    ✨ Feat : 커피챗 검색, 커피챗 삭제 ui 생성

--- src/components/common/card/CoffeeChatCard/CardBody.jsx
+++ src/components/common/card/CoffeeChatCard/CardBody.jsx
@@ -5,6 +5,7 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import { formattedDate, formattedTime } from "../../../../utils/formatDate";
 import { cardBodyStyle } from "./CoffeeChatCardStyle";
+import { useState } from "react";
 
 function CardBody({ data }) {
   const InfoValue = [
@@ -32,17 +33,55 @@ function CardBody({ data }) {
       <Typography color="#545459" fontWeight="600" sx={cardBodyStyle.title}>
         {data.title}
       </Typography>
-      <Box sx={cardBodyStyle.infoBox}>
-        {InfoValue.map((item, index) => (
-          <CoffeeInfoItem
-            key={index}
-            Icon={item.icon}
-            text={item.text}
-            color={item.color}
+      <Box display="flex">
+        <Box sx={cardBodyStyle.infoBox}>
+          {InfoValue.map((item, index) => (
+            <CoffeeInfoItem
+              key={index}
+              Icon={item.icon}
+              text={item.text}
+              color={item.color}
+            />
+          ))}
+        </Box>
+        <Box marginTop={10}>
+          <CommonButton
+            title="삭제"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
           />
-        ))}
+        </Box>
       </Box>
     </Box>
+  );
+}
+function CommonButton({ title, onClick }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const defaultStyle = {
+    border: "none",
+    cursor: "pointer",
+    borderRadius: "999px",
+    fontSize: "12px",
+    padding: "4px 10px",
+    background: "black",
+    color: "white",
+    fontWeight: 600,
+  };
+
+  const hoverStyle = {
+    ...defaultStyle,
+    background: "grey",
+  };
+  return (
+    <button
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={isHovered ? hoverStyle : defaultStyle}
+    >
+      {title}
+    </button>
   );
 }
 
