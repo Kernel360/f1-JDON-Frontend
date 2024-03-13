e633e54c89ce91e28dde1d31032cb3f1dff427e2
Wlfjd <leejiwon813@naver.com>
2024-03-13 17:48:31 +0900

    ✨ Feat :  유저 정보 박스 생성

---
+++ src/pages/coffeechats/create-coffeechats/styles.js
@@ -0,0 +1,25 @@
+export const infoBoxContainer = {
+  borderRadius: "10px",
+  width: "100%",
+  heigth: "64px",
+  background: "#F3F5FF",
+  py: "16px",
+  px: "16px",
+  display: "flex",
+  flexDirection: "column",
+  gap: 1,
+};
+
+export const infoTitle = {
+  p: 0,
+  fontSize: "12px",
+  alignItems: "center",
+  color: "#545459",
+  fontWeight: 600,
+};
+export const noticeMsg = {
+  p: 0,
+  fontSize: "12px",
+  alignItems: "center",
+  color: "#69696E",
+};
