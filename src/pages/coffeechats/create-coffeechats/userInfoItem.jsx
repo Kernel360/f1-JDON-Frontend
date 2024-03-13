810db9461398c3bedf86eac6e8deb3f5eb685c07
Wlfjd <leejiwon813@naver.com>
2024-03-13 17:54:22 +0900

    🐛 Fix :  충돌 해결

--- src/pages/coffeechats/create-coffeechats/userInfoItem.jsx
+++ src/pages/coffeechats/create-coffeechats/userInfoItem.jsx
@@ -1,15 +1,15 @@
-const { Box } = require("@mui/material");
+const { Box } = require('@mui/material');
 
 function infoItem({ title, content }) {
   return (
     <Box
       sx={{
-        pb: "5px",
-        color: "#696969",
-        fontSize: "12px",
-        display: "flex",
-        alignItems: "center",
-        gap: "8px",
+        pb: '5px',
+        color: '#696969',
+        fontSize: '12px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px',
       }}
     >
       {`${title} : ${content}`}
