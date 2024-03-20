b22f978e28aaac79a5945ccc7091ca4c4226c84d
junkue20 <junkue13@gmail.com>
2024-03-20 18:13:43 +0900

    ✨ Feat : 검색어 reset버튼 및 유효성 검사 메세지 추가

--- src/components/common/search-bar/SearchBarStyles.js
+++ src/components/common/search-bar/SearchBarStyles.js
@@ -1,6 +1,8 @@
 export const SearchTextFiled = () => ({
+  position: 'relative',
   background: '#F5F5F7',
   borderRadius: '10px',
+  height: '52px',
   margin: '12px auto',
   '& .MuiOutlinedInput-root': {
     margin: '2px 0',
@@ -12,8 +14,20 @@ export const SearchTextFiled = () => ({
 });
 
 export const SearchContainer = () => ({
+  zIndex: 'auto',
+  position: 'relative',
   padding: '0 !important',
   '& .MuiInputBase-input': {
     padding: '12px',
   },
+});
+
+export const SearchResetBtn = () => ({
+  position: 'absolute',
+  left: '0',
+  bottom: '-10%',
+
+  color: '#f84d4d',
+
+  fontWeight: '300',
 });
