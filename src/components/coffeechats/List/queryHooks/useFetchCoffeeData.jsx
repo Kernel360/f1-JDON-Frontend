d10854508cb70fee307f508816d48483e478f17a
Wlfjd <leejiwon813@naver.com>
2024-03-22 13:42:51 +0900

    📦️ Chore :  react-query 세팅

--- src/components/coffeechats/List/queryHooks/useFetchCoffeeData.jsx
+++ src/components/coffeechats/List/queryHooks/useFetchCoffeeData.jsx
@@ -1,37 +1,29 @@
-import { useEffect, useState } from 'react';
-
 import { getCoffeeChat } from 'api/api';
-import { useRecoilState } from 'recoil';
-import { coffeeChatListState } from 'recoil/atoms';
+import { useQuery } from 'react-query';
 
 const useFetchCoffeeData = (currentPage, sortData, 검색어) => {
-  const [coffeeData, setCoffeeData] = useRecoilState(coffeeChatListState);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const fetchCoffeeData = async () => {
+    const response = await getCoffeeChat(
+      currentPage - 1,
+      sortData.pageSize || 12,
+      sortData.sort,
+      sortData.jobCategory,
+      검색어,
+    );
+    return response.data.data;
+  };
 
-  useEffect(() => {
-    const fetchCoffeeData = async () => {
-      setLoading(true);
-      try {
-        const data = await getCoffeeChat(
-          currentPage - 1,
-          sortData.pageSize || 12,
-          sortData.sort,
-          sortData.jobCategory,
-          검색어,
-        );
-        setCoffeeData(data.data.data);
-        setError(null);
-      } catch (error) {
-        setError(error);
-        setCoffeeData(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCoffeeData();
-  }, [currentPage, sortData, 검색어]);
+  const {
+    data: coffeeData,
+    isLoading: loading,
+    error,
+  } = useQuery(
+    ['coffeeData', currentPage, sortData, 검색어], // 이 배열은 쿼리 키로 사용되며, 이 값들이 변경될 때마다 쿼리가 다시 실행됩니다.
+    fetchCoffeeData, // 데이터를 가져오는 함수
+    {
+      keepPreviousData: true,
+    },
+  );
 
   return { coffeeData, loading, error };
 };
