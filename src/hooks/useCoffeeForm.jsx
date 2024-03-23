196e0d66d16be745d71da888bf03559f2057048a
Wlfjd <leejiwon813@naver.com>
2024-03-23 09:41:14 +0900

    🔨 Modify:  경로 오타 수정

--- src/hooks/useCoffeeForm.jsx
+++ src/hooks/useCoffeeForm.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import useValidateForm from './useValidateForam';
+import useValidateForm from './useValidateForm';
 
 export const useCoffeeForm = (initialValues) => {
   const createFormValue = (values) => ({
