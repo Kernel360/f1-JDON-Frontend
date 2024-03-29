49cd8b967c4f652dfe3a17a72be51020aa9d5e7d
junkue20 <junkue17@naver.com>
2024-03-29 11:26:35 +0900

    🚑Hotfix : JD상세페이지 로그인 안되는 이슈

--- src/pages/jd-detail/JdDetail.jsx
+++ src/pages/jd-detail/JdDetail.jsx
@@ -2,8 +2,15 @@ import { Container, CssBaseline } from '@mui/material';
 import Header from 'components/common/header/Header';
 import { CategoryTab } from './CategoryTab';
 import { JD_CHILD } from 'constants/headerProps';
+import { useEffect } from 'react';
+import { useAuth } from 'pages/mainpage/hooks/useAuth';
 
 export function JdDetail() {
+
+  useEffect(() => {
+    useAuth;
+  }, []);
+
   return (
     <Container maxWidth="md">
       <CssBaseline />
