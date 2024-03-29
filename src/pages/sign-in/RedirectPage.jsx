5c4f34cfbafcd09a53218656186d197192d927bf
junkue20 <junkue17@naver.com>
2024-03-29 12:54:37 +0900

    🚨 Fix : 로그인 후 메인페이지로 이동 fix

--- src/pages/sign-in/RedirectPage.jsx
+++ src/pages/sign-in/RedirectPage.jsx
@@ -2,15 +2,12 @@ import { useEffect } from 'react';
 
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
-import { isLoggedInState, userInfo } from 'recoil/atoms';
+import { userInfo } from 'recoil/atoms';
 
 const RedirectPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const setData = useSetRecoilState(userInfo);
-  const setIsLogin = useSetRecoilState(isLoggedInState).isLoginUser;
-
-
   let path = localStorage.getItem('pathname');
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -22,15 +19,14 @@ const RedirectPage = () => {
       navigate('/info');
       setData((prev) => ({ ...prev, encrypted: value, hmac: hmac }));
     } else {
-      if (path) {
-        setIsLogin(true);
-        navigate(path);
-        localStorage.removeItem('pathname');
-      } else {
-        navigate('/');
-      }
+      // if (path) {
+      //   navigate(path);
+      //   // localStorage.removeItem('pathname');
+      // } else {
+      navigate('/');
+      // }
     }
-  }, [location, navigate, setData, path, setIsLogin]);
+  }, [location, navigate, setData, path]);
 
   return (
     <div>
